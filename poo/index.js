a70f309788d70316ee4fe85cf18127acf426a1c6
ASobrinoB <telemensajes@hotmail.com>
2024-04-09 13:46:14 -0400

    retomando hoy martes 3er break

--- poo/index.js
+++ poo/index.js
@@ -1,49 +1,42 @@
 class Encuesta {
-  constructor(pregunta, respuesta, opcionCorrecta, opcionUsuario) {
+  constructor(pregunta, respuesta, opcionCorrecta) {
     this.pregunta = pregunta;
     this.respuesta = respuesta;
     this.opcionCorrecta = opcionCorrecta;
-    this.opcionUsuario = opcionUsuario;
   }
 }
 
 totalEncuestas = 0;
 let encuestas = [];
 let respuestas = [];
+let opcionCorrectas = 0;
 
 do {
-  console.log("Ingreso de Encuestas");
-  console.log("--------------------");
-  console.log("\n");
-  console.log(`Encuestas registradas: ${totalEncuestas}`);
-  console.log("\n");
-
   preguntas = prompt(`Digite la PREGUNTA nro. ${totalEncuestas + 1} de la ENCUESTA\nsino presione\n<ENTER> para TERMINAR`);
 
   if (preguntas !== null && preguntas !== ""){
-    index = 1;
-    salida = false;
 
-    do {
-      respuestas [index - 1] = prompt(`Ingrese la RESPUESTA para la\nOPCION ${index}\na la PREGUNTA: ${preguntas}\nsino presione\n<ENTER> para volver a realizar la PREGUNTA`);
-      
-      if (respuestas [index - 1] === null || respuestas [index - 1] === ""){
-         salida = true;
-      } else {
-        index++;
-      }
-    } while (index <= 4 && salida == false)
+    salida = false
+    for (index = 0; index < 4; index++){
+      respuestas [index] = prompt(`Ingrese la RESPUESTA para la\nOPCION ${index + 1}\na la PREGUNTA: ${preguntas}\nsino presione\n<ENTER> para volver a realizar la PREGUNTA`);
+      if (respuestas [index] === null || respuestas [index] === ""){
+        index = 4;
+        salida = true;
+      }  
+    }    
 
-    if (salida == false) {  
+    if (!salida) {  
       do {
         opcionCorrectas = parseInt(prompt(`Ingrese la opción correcta de la PREGUNTA: ${preguntas}\n1 -> ${respuestas[0]}\n2 -> ${respuestas[1]}\n3 -> ${respuestas[2]}\n4 -> ${respuestas[3]}\nsino presione\n<ENTER> para volver a realizar la PREGUNTA`));
       } while (opcionCorrectas < 1 || opcionCorrectas > 4);
 
-      if (opcionCorrectas !== null && opcionCorrectas !== ""){
-         encuestas.push (new Encuesta (preguntas, respuestas, opcionCorrectas, 0));
+      if (opcionCorrectas >= 1 && opcionCorrectas <= 4){
+         encuestas.push (new Encuesta (preguntas, respuestas, opcionCorrectas));
          totalEncuestas++;
       } 
     }
   }
 } while (preguntas !== null && preguntas !== "");
+
+encuestas.forEach((value) => console.log(value));
 
