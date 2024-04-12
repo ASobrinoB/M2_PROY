d44a7bf8300586dbd0296e83c0d8b36be6c1d5c2
ASobrinoB <telemensajes@hotmail.com>
2024-04-11 22:48:38 -0400

    retomando hoy miercoles 4to break

--- poo/index.js
+++ poo/index.js
@@ -6,45 +6,63 @@ class Encuesta {
   }
 }
 
-let totalEncuestas = 0;
-const encuestas = [];
+var totalEncuestas = 0;
+var encuestas = [];
 
 do {
-
-  let preguntas = prompt(`Digite la PREGUNTA número ${totalEncuestas + 1} de la encuesta\n\npresione ENTER o el botón CANCELAR para TERMINAR\n`);
-
-  if (preguntas == null || preguntas == ""){
+  var preguntas = "";
+  var alertas = "";
+  var respuestas = ["", "", "", ""];
+  var opcionCorrectas = 0;
   
-    if (totalEncuestas <= 8){
-       let alerta = prompt(`Debe ingresar al menos 8 preguntas para la encuesta\n\n¿Está seguro que desea TERMINAR?\n\nDigite SI y presione ENTER o ACEPTAR para TERMINAR`);
-  
-       if (alerta !== "SI"){
-          let respuestas = ["", "", "", ""];
-          let salida = false;
+  preguntas = prompt(`Digite la PREGUNTA número ${totalEncuestas + 1} de la encuesta\n\npresione ENTER o el botón CANCELAR para TERMINAR\n`);
+  alert (preguntas);
 
-          for (let index = 0; index < 4; index++){
-             respuestas[index] = prompt(`Digite la RESPUESTA para la opción [${index + 1}] a la pregunta:\n\n¿${preguntas}?\n\nopción 1 -> ${respuestas[0]}\nopción 2 -> ${respuestas[1]}\nopción 3 -> ${respuestas[2]}\nopción 4 -> ${respuestas[3]}\n\npresione ENTER o el botón CANCELAR\npara volver a la PREGUNTA de la encuesta\n`);
-             if (respuestas[index] === null || respuestas[index] === ""){
-                salida = true;
-                index = 4;
-             };
-          };
+  if (preguntas !== null && preguntas !== ""){  
+     respuestas = ["", "", "", ""];
+          
+     for (let index = 0; index < 4; index++){
+         respuestas[index] = prompt(`Digite la RESPUESTA para la opción [${index + 1}] a la pregunta:\n\n¿${preguntas}?\n\nopción 1 -> ${respuestas[0]}\nopción 2 -> ${respuestas[1]}\nopción 3 -> ${respuestas[2]}\nopción 4 -> ${respuestas[3]}\n\npresione ENTER o el botón CANCELAR\npara volver a la PREGUNTA de la encuesta\n`);
+         alert (respuestas[index]);
+             
+         if (respuestas[index] === null || respuestas[index] === ""){
+            break;
+         }
+     }
 
-          if (!salida){  
-             let opcionCorrectas;
-  
-             do {
-                opcionCorrectas = parseInt(prompt(`Digite la OPCION donde se encuentra\nla RESPUESTA CORRECTA a la pregunta:\n\n¿${preguntas}?\n\nopción 1 -> ${respuestas[0]}\nopción 2 -> ${respuestas[1]}\nopción 3 -> ${respuestas[2]}\nopción 4 -> ${respuestas[3]}\n\npresione ENTER o el botón CANCELAR\npara volver a la PREGUNTA de la encuesta\n`));
-             } while (opcionCorrectas < 1 || opcionCorrectas > 4);
+     if (respuestas[3] !== null && respuestas[3] !== ""){  
+        do {
+          opcionCorrectas = parseInt(prompt(`Digite la OPCION donde se encuentra\nla RESPUESTA CORRECTA a la pregunta:\n\n¿${preguntas}?\n\nopción 1 -> ${respuestas[0]}\nopción 2 -> ${respuestas[1]}\nopción 3 -> ${respuestas[2]}\nopción 4 -> ${respuestas[3]}\n\npresione ENTER o el botón CANCELAR\npara volver a la PREGUNTA de la encuesta\n`));
+        } while (opcionCorrectas < 1 || opcionCorrectas > 4)
 
-             if (opcionCorrectas >= 1 && opcionCorrectas <= 4){
-                encuestas.push(new Encuesta(preguntas, respuestas, opcionCorrectas));
-                totalEncuestas++;
-             };
-          };
-        };
-      };
-    };
+        alert (opcionCorrectas);
+
+        if (opcionCorrectas >= 1 && opcionCorrectas <= 4){
+           encuestas.push(new Encuesta(preguntas, respuestas, opcionCorrectas));
+           totalEncuestas++;
+
+           alert (totalEncuestas)
+         }
+      }
+   } else {
+        if (totalEncuestas <= 8){
+           alertas = prompt(`Debe ingresar al menos 8 preguntas para la encuesta\n\n¿Está seguro que desea TERMINAR?\n\nDigite SI y presione ENTER o ACEPTAR para TERMINAR`);
+           alert (alertas);
+
+           if (alertas === null){
+              preguntas = "volver al do-while";
+              alert (preguntas);
+            } else {
+              alertas = alertas.toUpperCase()
+              if (alertas !== "SI"){
+                preguntas = "volver al do-while";
+                alert (preguntas);
+              }
+            }
+           }           
+   }
+   
+   alert (preguntas);
 
 } while (preguntas !== null && preguntas !== "")
 
